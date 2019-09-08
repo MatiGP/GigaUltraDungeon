cf4973817df4a4e3f5bb6f34c5fa1e1818b524c8
SoopSoop <mateusz.swieca@o2.pl>
2019-09-08 11:18:49 +0200

    Scripts, sprites
    > New script
    > Edited some sprites

---
+++ PixelLoot/Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 direction = new Vector2(Input.GetAxis("Vertical"),0);
+
+        rb2d.MovePosition(rb2d.transform.position + direction);
+    }
+}
--- PixelLoot/Assets/Scripts/ScriptableObject/PlayableChar_SO.cs
+++ PixelLoot/Assets/Scripts/ScriptableObject/PlayableChar_SO.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class PlayableChar_SO : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string characterName;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public int characterBaseHealth {
+        get {
+            return characterBaseHealth;
+        }
+        set {
+            characterBaseHealth = value;
+        }
     }
 }
