27d13fd13bb184972a0865987c8f3ea9725226b2
MatiGP <mateusz.swieca@o2.pl>
2019-11-12 07:20:23 +0100

    12.11

---
+++ PixelLoot/Assets/Scripts/BoardCreatorTiles.cs
@@ -0,0 +1,207 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class BoardCreatorTiles : MonoBehaviour
+{
+    public enum TileType
+    {
+        Wall, Floor,
+    }
+    [Space(2)]
+    [Header("Wymiary Planszy")]
+    public int columns = 100;
+    public int rows = 100;
+    [Space(2)]
+    [Header("Pomieszczenia i korytarze")]
+    public IntRange numRooms = new IntRange(5, 10);
+    public IntRange roomWidth = new IntRange(3, 10);
+    public IntRange roomHeight = new IntRange(3, 10);
+    public IntRange corridorLength = new IntRange(5, 7);
+    [Space(2)]
+    [Header("Tiles")]
+    public Tile[] floorTiles;
+    public Tile[] wallTiles;
+    public Tile outerWallTile;
+    public Tilemap playArea;
+    [Space(2)]
+    [Header("Characters, creatures, tresure to spawn")]
+    public GameObject[] playableChars;
+    public GameObject[] startWeapons;
+    public GameObject[] enemies;
+
+    private TileType[][] tiles;
+    private Room[] rooms;
+    private Corridor[] corridors;
+
+    private void Start()
+    {
+        playArea.ClearAllTiles();
+
+        SetupTilesArray();
+
+        CreateRoomsAndCorridors();
+
+        SetTilesValuesForRooms();
+        SetTilesValueForCorridors();
+
+        InstantiateTiles();
+        InstantiateOuterWalls();
+
+    }
+
+    void SetupTilesArray()
+    {
+        tiles = new TileType[columns][];
+        for (int i = 0; i < tiles.Length; i++)
+        {
+            tiles[i] = new TileType[rows];
+        }
+    }
+    void CreateRoomsAndCorridors()
+    {
+        rooms = new Room[numRooms.Random];
+
+        corridors = new Corridor[rooms.Length - 1];
+
+        rooms[0] = new Room();
+        corridors[0] = new Corridor();
+
+        rooms[0].SetupRoom(roomWidth, roomHeight, columns, rows);
+
+        corridors[0].SetupCorridor(rooms[0], corridorLength, roomWidth, roomHeight, columns, rows, true, rooms);
+
+        Vector3 playerPos = new Vector3(rooms[0].xPos, rooms[0].yPos, 0f);
+        Instantiate(playableChars[PlayerPrefs.GetInt("selectedChar") - 1], playerPos, Quaternion.identity);
+        Instantiate(startWeapons[PlayerPrefs.GetInt("selectedChar") - 1], new Vector3(playerPos.x + 2, playerPos.y + 2), Quaternion.identity);
+
+        for (int i = 1; i < rooms.Length; i++)
+        {
+            rooms[i] = new Room();
+
+            rooms[i].SetupRoom(roomWidth, roomHeight, columns, rows, corridors[i - 1], rooms);
+
+            if (i < corridors.Length)
+            {
+                corridors[i] = new Corridor();
+
+                corridors[i].SetupCorridor(rooms[i], corridorLength, roomWidth, roomHeight, columns, rows, false, rooms);
+            }
+            int rand = Random.Range(0, enemies.Length);
+            int randX = Random.Range(rooms[i].xPos, rooms[i].xPos + rooms[i].roomWidth);
+            int randY = Random.Range(rooms[i].yPos, rooms[i].yPos + rooms[i].roomHeight);
+            Vector3 enemyPos = new Vector3(randX, randY);
+            Instantiate(enemies[rand], enemyPos, Quaternion.identity);
+
+        }
+    }
+    void SetTilesValuesForRooms()
+    {
+        for (int i = 0; i < rooms.Length; i++)
+        {
+            Room currentRoom = rooms[i];
+
+            for (int j = 0; j < currentRoom.roomWidth; j++)
+            {
+                int xCoord = currentRoom.xPos + j;
+
+                for (int k = 0; k < currentRoom.roomHeight; k++)
+                {
+                    int yCoord = currentRoom.yPos + k;
+
+                    tiles[xCoord][yCoord] = TileType.Floor;
+                }
+            }
+        }
+    }
+    void SetTilesValueForCorridors()
+    {
+        for (int i = 0; i < corridors.Length; i++)
+        {
+            Corridor currentCorridor = corridors[i];
+
+            for (int j = 0; j < currentCorridor.corridorLength; j++)
+            {
+                int xCoord = currentCorridor.startXPos;
+                int yCoord = currentCorridor.startYPos;
+
+                switch (currentCorridor.direction)
+                {
+                    case Direction.North:
+                        yCoord += j;
+                        break;
+                    case Direction.East:
+                        xCoord += j;
+                        break;
+                    case Direction.South:
+                        yCoord -= j;
+                        break;
+                    case Direction.West:
+                        xCoord -= j;
+                        break;
+                }
+
+                tiles[xCoord][yCoord] = TileType.Floor;
+            }
+        }
+    }
+    void InstantiateTiles()
+    {
+        for (int i = 0; i < tiles.Length; i++)
+        {
+            for (int j = 0; j < tiles[i].Length; j++)
+            {
+                if (tiles[i][j] == TileType.Wall)
+                {
+                    //InstantiateFromArray(wallTiles, i, j);
+
+                    playArea.SetTile(new Vector3Int(i, j, 0), wallTiles[Random.Range(0, wallTiles.Length)]);
+                }
+                else
+                {
+                    //InstantiateFromArray(floorTiles, i, j);
+                    playArea.SetTile(new Vector3Int(i, j, 0), floorTiles[Random.Range(0, floorTiles.Length)]);
+                }
+            }
+        }
+    }
+
+    void InstantiateOuterWalls()
+    {
+        float leftEdgeX = -1f;
+        float rightEdgeX = columns + 0f;
+        float bottomEdgeY = -1f;
+        float topEdgeY = rows + 0f;
+
+        InstantiateVerticalOuterWall(leftEdgeX, bottomEdgeY, topEdgeY);
+        InstantiateVerticalOuterWall(rightEdgeX, bottomEdgeY, topEdgeY);
+
+        InstantiateHorizontalOuterWall(leftEdgeX + 1f, rightEdgeX - 1f, bottomEdgeY);
+        InstantiateHorizontalOuterWall(leftEdgeX + 1f, rightEdgeX - 1f, topEdgeY);
+    }
+
+    void InstantiateHorizontalOuterWall(float startingX, float endingX, float yCoord)
+    {
+        float currentX = startingX;
+
+        while (currentX <= endingX)
+        {
+            //InstantiateFromArray(outerWallTiles, currentX, yCoord);
+            playArea.SetTile(new Vector3Int((int)currentX, (int)yCoord, 0), outerWallTile);
+            currentX++;
+        }
+    }
+
+    void InstantiateVerticalOuterWall(float xCoord, float startingY, float endingY)
+    {
+        float currentY = startingY;
+
+        while (currentY <= endingY)
+        {
+            playArea.SetTile(new Vector3Int((int)xCoord, (int)currentY, 0), outerWallTile);
+            //InstantiateFromArray(outerWallTiles, xCoord, currentY);
+            currentY++;
+        }
+    }
+}
